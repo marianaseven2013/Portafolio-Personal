aa4eeb5fdb3e2a320dea9a84e60d9e36bec756c1
Genesis <gmluch@scl.edu.gt>
2024-09-26 10:05:04 -0600

    Segunda versión de portafolio -Se agrega el navBar -Se agrega el header -Se agrega un elemento item de guia

--- Componentes/proyectos/proyectos.js
+++ Componentes/proyectos/proyectos.js
@@ -1,3 +1,5 @@
+import { header } from "../header/header.js";
+
 function proyectos(){
 
     let section = document.createElement('section');
@@ -10,21 +12,7 @@ function proyectos(){
 
 //Función header
 
-function header(){
-    let div = document.createElement('div');
-    div.className = "header";
 
-    let h2 = document.createElement('h2');
-    h2.innerHTML = "Proyectos"
-    div.appendChild(h2);
-
-    let btn = document.createElement('div')
-    btn.innerHTML = "GitHub"
-    div.appendChild(btn);
-
-   return div;
-  
-}
 //Item lista
 
 function lista(){
